2bcde1ec85856f2a9fba5f42bef1b71c1496f537
sh1h4b <sh1h4b@users.noreply.github.com>
2026-08-01 21:43:51 +0600

    Generate expressions and control flow

--- src/codegen/codegen.c
+++ src/codegen/codegen.c
@@ -31,3 +31,74 @@ static char *float_literal_str(double v) {
 }
 
 static void gen_stmt(Node *n);
+
+static char *gen_expr(Node *n) {
+    if (!n) return strdup("?");
+
+    switch (n->type) {
+        case ND_ID:
+            return strdup(n->resolved_name ? n->resolved_name : n->name);
+
+        case ND_INT_LIT:
+            return int_literal_str(n->int_val);
+
+        case ND_FLOAT_LIT:
+            return float_literal_str(n->float_val);
+
+        case ND_BOOL_LIT:
+            return strdup(n->bool_val ? "true" : "false");
+
+        case ND_UNOP: {
+            char *operand = gen_expr(n->left);
+            char *result = new_temp();
+            if (strcmp(n->op, "-") == 0)
+                printf("%s = uminus %s\n", result, operand);
+            else
+                printf("%s = %s %s\n", result, n->op, operand);
+            return result;
+        }
+
+        case ND_BINOP: {
+            char *l = gen_expr(n->left);
+            char *r = gen_expr(n->right);
+            char *result = new_temp();
+            printf("%s = %s %s %s\n", result, l, n->op, r);
+            return result;
+        }
+
+        default:
+            fprintf(stderr, "codegen: unexpected node %s used as expression\n",
+                    nodetype_str(n->type));
+            return strdup("?");
+    }
+}
+
+static void gen_if(Node *n) {
+    char *cond = gen_expr(n->left);
+    if (!n->third) {
+        char *l1 = new_label();
+        printf("ifFalse %s goto %s\n", cond, l1);
+        gen_stmt(n->right);
+        printf("%s:\n", l1);
+    } else {
+        char *l1 = new_label();
+        char *l2 = new_label();
+        printf("ifFalse %s goto %s\n", cond, l1);
+        gen_stmt(n->right);
+        printf("goto %s\n", l2);
+        printf("%s:\n", l1);
+        gen_stmt(n->third);
+        printf("%s:\n", l2);
+    }
+}
+
+static void gen_while(Node *n) {
+    char *l1 = new_label();
+    char *l2 = new_label();
+    printf("%s:\n", l1);
+    char *cond = gen_expr(n->left);
+    printf("ifFalse %s goto %s\n", cond, l2);
+    gen_stmt(n->right);
+    printf("goto %s\n", l1);
+    printf("%s:\n", l2);
+}
