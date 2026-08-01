db600e7954c51ae3532694c6610489c04b3ff937
sh1h4b <sh1h4b@users.noreply.github.com>
2026-08-01 17:46:29 +0600

    Add code generation helpers

--- src/codegen/codegen.c
+++ src/codegen/codegen.c
@@ -11,3 +11,23 @@ static char *new_temp(void) {
     snprintf(buf, 16, "t%d", ++temp_count);
     return buf;
 }
+
+static char *new_label(void) {
+    char *buf = (char *)malloc(16);
+    snprintf(buf, 16, "L%d", ++label_count);
+    return buf;
+}
+
+static char *int_literal_str(int v) {
+    char *buf = (char *)malloc(32);
+    snprintf(buf, 32, "%d", v);
+    return buf;
+}
+
+static char *float_literal_str(double v) {
+    char *buf = (char *)malloc(32);
+    snprintf(buf, 32, "%g", v);
+    return buf;
+}
+
+static void gen_stmt(Node *n);
